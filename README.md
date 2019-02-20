a2b3ffc07af42bdbfa1f3aca07ef7a15619b1764
wk-j <somnuk.wk@gmail.com>
2019-02-20 19:10:49 +0700

    Middleware

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Web API
 
 - Decode params from route
-
-Do not use [FromRoute] when values might contain %2f (that is /) because %2f won't be escaped to /. Use [FromQuery] if the value might contain %2f
+- Do not use [FromRoute] when values might contain %2f (that is /) because %2f won't be escaped to /. Use [FromQuery] if the value might contain %2f
--- src/UrlDecode/Startup.cs
+++ src/UrlDecode/Startup.cs
@@ -11,35 +11,33 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace UrlDecode
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace UrlDecode {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            } else {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) => {
+                var path = context.Request.Path;
+                Console.WriteLine("Configure - {0}", path);
+                await next();
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
